55ae0d32a0d3b68620918c559351caedd4c8b42a
Chandra1677 <cprasad858@gmail.com>
2026-06-03 13:02:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const text = [
+    "Full Stack Developer",
+    "Java Developer",
+    "React Developer",
+    "Web Developer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+        setTimeout(type, 1200);
+    }else{
+        setTimeout(type, 120);
+    }
+
+})();
